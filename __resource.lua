1f880cb89000c7002ad18f53489931460b33756f
SNR <75149094+sonerbeyss@users.noreply.github.com>
2023-05-19 07:27:02 +0300

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+files {
+    "*",
+    "img/*",
+    "img/back/*",
+    "img/fonts/*",
+    "img/haber/*",
+    "img/iletisim/*",
+    "img/main/*",
+    "js/*",
+    "snd/*"
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
